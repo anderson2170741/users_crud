9a4f773a3b94966334d7d0436e3a805dd36288d2
anderson2170741 <andersonduranpc@gmail.com>
2023-07-20 11:04:45 -0400

    ssl required

--- utils/database.js
+++ utils/database.js
@@ -8,6 +8,7 @@ const db = new Sequelize({
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     dialect: "postgres",
+    dialectOptions: {ssl: { requiere: true, rejectUnauthorized: false }}
 });
 
 /*const db2 = new Sequelize({
